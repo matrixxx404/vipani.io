cbf619dee8cb94577ddc489d4f9540ec3de9e6d7
matrixxx404 <ashishprasannan186@gmail.com>
2024-11-06 15:41:32 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+// Only import react-native-gesture-handler on native platforms
+import 'react-native-gesture-handler';
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
